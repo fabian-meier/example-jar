e47574f89bcbc3867b861e256aa888dac36ccb04
fabian-meier <89151926+fabian-meier@users.noreply.github.com>
2021-08-18 18:25:20 +0200

    POM erstellt

---
+++ src/main/java/de/beispiel/Main.java
@@ -0,0 +1,17 @@
+package de.beispiel
+
+/**
+ * Beispiel-Main
+ */
+public class Main
+{
+
+  /**
+   * @param args Argumente
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Hello Beispiel");
+  }
+
+}
